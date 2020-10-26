ef6d11921c2faeb719b90fd346507a5239ee3f95
Brendan Lane <me@imbl.me>
2020-10-26 00:11:57 -0400

    🔃 Sync with local

--- bot/pluto.js
+++ bot/pluto.js
@@ -1,32 +1,44 @@
 // Pluto - bot/bot.ts
 // Written by Brendan Lane - https://github.com/brendan-lane
+// Define necessary imports & variables
+// @ts-ignore
+/*
+import Discord from "discord.js";
 import config from "./config.js";
 import Commando from "discord.js-commando";
 import path from "path";
+
 const client = new Commando.CommandoClient({
     commandPrefix: config.prefix,
     owner: config.owner,
     invite: config.supportServer
 });
+
 client.registry
     .registerDefaultTypes()
     .registerDefaultGroups()
     .registerDefaultCommands()
-    .registerCommandsIn(path.join(process.cwd(), "bot/modules/commands"));
+
 // On start of bot
 client.once("ready", () => {
     // Initial Startup Messages
     console.log(`[Pluto] Logged in as ${client.user.tag}`);
     if (client.user.bot) {
         console.log("[Pluto] ✔ This user is a bot user");
-    }
-    else if (!client.user.bot) {
+    } else if (!client.user.bot) {
         console.log("[Pluto] ✘ This user is not a bot user");
+    } else {
+        console.log("[Pluto] For some reason, The system cannot tell if the discord user is a bot or not.")
     }
-    else {
-        console.log("[Pluto] For some reason, The system cannot tell if the discord user is a bot or not.");
-    }
+
     // Set presence to Streaming with carpel tunnel
-    client.user.setPresence({ activity: { name: 'with carpal tunnel', type: "STREAMING", url: "https://www.twitch.tv/smallkidwithgun" } });
+    client.user.setPresence({ activity: { name: 'with carpal tunnel', type: "STREAMING", url: "https://www.twitch.tv/smallkidwithgun" }});
 });
+
 client.login(config.token);
+
+*/
+import BotInstance from "./BotInstance.js";
+import config from "./config.js";
+const pluto = new BotInstance.Pluto(config.token);
+pluto.start();
