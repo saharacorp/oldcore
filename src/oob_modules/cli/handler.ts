435a7f83f9fcd928ad92e141b6bbb117aca0adb3
Brendan Lane <me@brndnln.dev>
2021-01-24 03:24:58 -0500

    Change out directory

--- src/oob_modules/cli/handler.ts
+++ src/oob_modules/cli/handler.ts
@@ -1,2 +1,10 @@
-// Pluto - src/
-//
+// Pluto - src/oob_modules/cli/handler.ts
+// Written by Brendan Lane - https://brndnln.dev/
+
+import { Command } from 'commander'
+import config from '../../configuration/config'
+
+const program = new Command()
+program.version(config.cli.version)
+
+program
