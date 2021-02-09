dba33f621aca430b23b7a59e5a7e503926937e6d
Brendan Lane <me@brndnln.dev>
2021-02-09 17:39:58 -0500

    Continue CLI Handler

--- src/oob_modules/cli/shell/handler.ts
+++ src/oob_modules/cli/shell/handler.ts
@@ -1,15 +1,31 @@
 // Pluto - src/oob_modules/cli/shell/handler.ts
 // Written by Brendan Lane - https://brndnln.dev/
 
-import { prompt } from 'enquirer'
+import { createInterface } from 'readline'
+import { readFileSync } from 'fs'
 import config from '../../../configuration/config'
+import { log, loggerType } from '../logger'
 
 export default async function shellHandler () {
-  const command = await prompt({
-    type: 'input',
-    name: 'commandInput',
-    message: config.cli.prompt
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
   })
 
-  console.log(command)
+  rl.question(config.cli.prompt, (command: string) => {
+    switch (command) {
+      case 'close':
+        log(loggerType.INFO, 'Closing CLI prompt...')
+        rl.close()
+				break
+
+			case 'help':
+				console.log(readFileSync('./messages/cliHelp.txt', { encoding: "utf-8" }))
+				break
+    }
+  })
+
+	rl.on("close", () => {
+		log(loggerType.OK, 'CLI prompt closed')
+	})
 }
