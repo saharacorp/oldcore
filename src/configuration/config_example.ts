0d7c25907f95b42695e4830217d53c85b5e64e25
Brendan Lane <me@brndnln.dev>
2021-02-27 16:07:13 -0500

    Update config_example.ts

--- src/configuration/config_example.ts
+++ src/configuration/config_example.ts
@@ -1,27 +1,43 @@
-// Pluto - bot/config.js
+// Pluto - bot/configuration/config.ts
 // Written by Brendan Lane - https://brndnln.dev/
 
+import { bold, red, gray } from 'colors'
+
 export default {
+  // CLI Configuration
+  cli: {
+    version: '1.3.8',
+		prompt: `${bold(`${red('(local)')} ${gray('$ » ')}`)}`
+  },
+  // Bot Configuration
   bot: {
     // Prefix
     prefix: '.',
     // Owner ID
     ownerid: '',
-    // Bot token
+    // Bot tokens
     token: '',
+    canaryToken: '',
     // Bot Personalization
     personalization: {
       botName: 'Pluto',
       embed: {
-        name: '',
-        image: '',
-        link: ''
+        name: 'Pluto',
+        image: 'https://media.discordapp.net/attachments/800944984500600855/803110348852953108/download_31.jpg?width=666&height=666',
+        link: 'https://github.com/brndnln/Pluto'
       }
     }
   },
+  // Administration Site Options
   adminSite: {
+    // Port for website
     port: 8000,
+    // Authentication
     username: 'admin',
     password: 'admin'
+  },
+  // Development Options (do not touch unless you know what you are doing)
+  devOptions: {
+    debugBuild: false,
+    canary: false
   }
-}
