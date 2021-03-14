afa450e6df7cb0a89641e6e255e4ba498b4a2e7b
Quinn Lane <me@brndnln.dev>
2021-03-14 05:41:19 -0400

    Finish argument handler 🎉

--- src/start.ts
+++ src/start.ts
@@ -1,17 +1,15 @@
 // Pluto - src/start.ts
 // Written by Quinn Lane - https://brndnln.dev/
 
-import { readFileSync } from 'fs'
-import { getArrayPos, rainbow } from './oob_modules/misc/jst'
-
 import { log, loggerType } from './oob_modules/cli/logger'
 import Pluto from './bot/pluto'
 
 import { Command, Option } from 'commander'
 import config from './configuration/config'
+import { getArrayPos, rainbow } from './oob_modules/misc/jst'
+import { readFileSync } from 'fs'
 
 const args = process.argv.slice(2)
-let argPos: number
 let bannerPos: number
 
 const banners = [
@@ -96,12 +94,69 @@ if (!args.includes('--no-start')) {
 */
 
 const program = new Command()
+
+const debug = new Option('-d, --debug', 'Shows JSON of all arguments passed as a debug measure')
+  .hideHelp()
+  .default(false)
+
+const banner = new Option('--banner <banner>', 'Forces a custom banner to be used')
+  .default('random')
+  .choices([
+    'random',
+    ...banners
+  ])
+
+const showBanners = new Option('--show-banners', 'Shows all the banners that can be used')
+  .default(false)
+
+const rainbowArg = new Option('--rainbow, --gaytime', 'Makes the text gay (it turns the text rainbow)')
+  .hideHelp()
+  .default(false)
+
+const noStart = new Option('--no-start', 'Do not start the bot, just run the console')
+  .default(true)
+
 program
-	.version(config.cli.version, '-v, --version', 'Shows the version of PlutoCore')
-	.addOption(new Option('--banner <banner>', 'Forces a custom banner to be used'))
-	.addOption(new Option('--show-banners', 'Shows all the banners that can be used'))
-	.addOption(new Option('--rainbow, --gaytime', 'Makes the text gay (it turns the text rainbow)').hideHelp())
-	.addOption(new Option('--no-start', 'Do not start the bot, just run the console'))
-	.parse(process.argv)
+  .version(config.cli.version, '-v, --version', 'Shows the version of PlutoCore')
+  .addOption(debug)
+  .addOption(banner)
+  .addOption(showBanners)
+  .addOption(rainbowArg)
+  .addOption(noStart)
+  .parse(process.argv)
 
 const options = program.opts()
+
+if (options.debug) console.log(options)
+if (options.showBanners) {
+  console.log('\n                           THE PLUTO DISCORD BOT\n       Written by Quinn Lane - https://brndnln.dev/ https://pluto.rip/\n')
+  console.log('\n       List of all possible banners:\n')
+  banners.forEach(value => {
+    bannerPos = getArrayPos(value, banners) + 1
+    console.log(`       [${bannerPos.toString()} of ${banners.length}] ${value}:`)
+    if (options.gaytime) {
+      rainbow(readFileSync(`./messages/banners/${value}.txt`, { encoding: 'utf-8' }))
+    } else {
+      console.log(readFileSync(`./messages/banners/${value}.txt`, { encoding: 'utf-8' }))
+    }
+  })
+  process.exit(0)
+}
+if (options.banner !== 'random') {
+  if (options.gaytime) {
+    rainbow(`\n${readFileSync(`./messages/banners/${options.banner}.txt`, { encoding: 'utf-8' })}`)
+    rainbow('\n                           THE PLUTO DISCORD BOT\n       Written by Quinn Lane - https://brndnln.dev/ https://pluto.rip/\n')
+  } else {
+    console.log(`\n${readFileSync(`./messages/banners/${options.banner}.txt`, { encoding: 'utf-8' })}`)
+    console.log('\n                           THE PLUTO DISCORD BOT\n       Written by Quinn Lane - https://brndnln.dev/ https://pluto.rip/\n')
+  }
+} else {
+  if (options.gaytime) {
+    rainbow(`\n${readFileSync(`./messages/banners/${banners[Math.floor(Math.random() * banners.length)]}.txt`, { encoding: 'utf-8' })}`)
+    rainbow('\n                           THE PLUTO DISCORD BOT\n       Written by Quinn Lane - https://brndnln.dev/ https://pluto.rip/\n')
+  } else {
+    console.log(`\n${readFileSync(`./messages/banners/${banners[Math.floor(Math.random() * banners.length)]}.txt`, { encoding: 'utf-8' })}`)
+    console.log('\n                           THE PLUTO DISCORD BOT\n       Written by Quinn Lane - https://brndnln.dev/ https://pluto.rip/\n')
+  }
+}
+if (options.start) Pluto()
